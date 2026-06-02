bfd6247981b0ff8b5a06cd36131d688c52fbea14
MuriloSobral17 <murilosobrallisboadasilva12@gmail.com>
2026-06-02 09:51:55 -0300

    Adiciona rota de listagem de livros

--- src/app.py
+++ src/app.py
@@ -23,6 +23,11 @@ def sobre():
     return "Sistema desenvolvido em Flask para estudo de CI/CD"
 
 
+@app.route("/livros")
+def livros():
+    return "Lista de livros cadastrados"
+
+
 if __name__ == "__main__":
     app.run(debug=True, port = 5001)
 
