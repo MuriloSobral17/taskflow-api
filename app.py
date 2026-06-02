92a84278c7655484eaebf528104ee6a1d770bc70
MuriloSobral17 <murilosobrallisboadasilva12@gmail.com>
2026-06-02 09:44:35 -0300

    feat: estrutura inicial da API com rota home

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return {"mensagem": "API funcionando corretamente"}
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port = 5001)
+
+
