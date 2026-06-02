8cd33c49741b7a0d15787018a45d3cc958e04198
MuriloSobral17 <murilosobrallisboadasilva12@gmail.com>
2026-06-02 09:51:31 -0300

    Atualiza descrição da página sobre

--- src/app.py
+++ src/app.py
@@ -18,6 +18,11 @@ def home():
     return "Sistema de Gerenciamento de Biblioteca"
 
 
+@app.route("/sobre")
+def sobre():
+    return "Sistema desenvolvido em Flask para estudo de CI/CD"
+
+
 if __name__ == "__main__":
     app.run(debug=True, port = 5001)
 
