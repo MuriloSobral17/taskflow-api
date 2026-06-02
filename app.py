eb86c76c9fcd873406e66ab14be2d504832fbf45
MuriloSobral17 <murilosobrallisboadasilva12@gmail.com>
2026-06-02 09:45:12 -0300

    feat: adiciona rota de status

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ def home():
     return {"mensagem": "API funcionando corretamente"}
 
 
+[]:
+
+
+@app.route("/status")
+def status():
+    return {"status": "API online"}
+
+
 if __name__ == "__main__":
     app.run(debug=True, port = 5001)
 
