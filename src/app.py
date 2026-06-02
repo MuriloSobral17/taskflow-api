25d386f10eb3c3e998a309e8edf8808e2ac4b374
MuriloSobral17 <murilosobrallisboadasilva12@gmail.com>
2026-06-02 09:52:14 -0300

    Adiciona rota de autores

--- src/app.py
+++ src/app.py
@@ -28,6 +28,11 @@ def livros():
     return "Lista de livros cadastrados"
 
 
+@app.route("/autores")
+def autores():
+    return "Lista de autores cadastrados"
+
+
 if __name__ == "__main__":
     app.run(debug=True, port = 5001)
 
