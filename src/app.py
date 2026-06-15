f083229dd8a4d1b1a180738cf384bad011e603cc
MuriloSobral17 <murilosobrallisboadasilva12@gmail.com>
2026-06-15 15:30:47 -0300

    chore: prepara aplicação para deploy

--- src/app.py
+++ src/app.py
@@ -38,7 +38,10 @@ def cadastro_livro():
     return "Página de cadastro de livros"
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, port = 5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
